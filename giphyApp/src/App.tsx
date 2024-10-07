e0b6975fce46505601afd7ac97c6643b19c142ac
Nicolás Willemet <nwillemet18@gmail.com>
2024-10-07 16:19:06 -0300

    Commit inicial

---
+++ giphyApp/src/App.tsx
@@ -0,0 +1,14 @@
+import { GiftList } from "./components/GiftList/GiftList";
+import { NavBar } from "./components/ui/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <GiftList/>
+    </>
+  );
+}
+
+export default App
